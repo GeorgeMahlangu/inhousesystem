dd63f1e8fe8b738c7caea85502253abf60eb328c
GeorgeMahlangu <givenmatibe@gmai.com>
2021-12-30 19:45:59 +0200

    Made changes to the Database API

--- admin/add_student_no_interview.php
+++ admin/add_student_no_interview.php
@@ -1,57 +1,44 @@
 <?php
-	include 'includes/session.php';
-	$conn = $pdo->open();
+include 'includes/session.php';
+$conn = $pdo->open();
 
-	if(isset($_POST['register'])){
-		
-		$initials = $_POST['initials'];
-        $lastname = $_POST['last_name'];
-        $studentNum = $_POST['student_number'];
-		$email = $_POST['email'];
-		$contact = $_POST['contact'];
-        $subjectCode = $_POST['subject_code'];
-        $reasonExclusion = $_POST['reason_exclusion'];
-        $condition = $_POST['condition_admission'];
-        $password = "12345678";
-        $interviewed = 1;
+if (isset($_POST['register'])) {
 
-		try
-        {
-				$stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows1 FROM student WHERE studentNum = :studentNumber");
-				$stmt->execute(['studentNumber'=>$studentNum]);
-				$row = $stmt->fetch();
+	$initials = $_POST['initials'];
+	$lastname = $_POST['last_name'];
+	$studentNum = $_POST['student_number'];
+	$email = $_POST['email'];
+	$contact = $_POST['contact'];
+	$subjectCode = $_POST['subject_code'];
+	$reasonExclusion = $_POST['reason_exclusion'];
+	$condition = $_POST['condition_admission'];
+	$password = "12345678";
+	$interviewed = 0;
 
-				if($row['numrows1'] > 0){
-					$_SESSION['error'] = 'Student already Exists';
-                    header('location: add-student-department.php');
-                    exit();
-					
-				}
-				else
-				{
-                    $stmt = $conn->prepare("INSERT INTO `student` (`studentNum`,`initials`, `lastname`, `email`,`contactNr`, `password`, `condition_reAdmission`, `reason`, `subjCode`, `interviewed`,`highSchool`, `Province`, `town` ) VALUES (:studentNum, :initials, :lastname, :email, :contact, :password, :condition, :reason, :subjCode, :interviewed, NULL, NULL, NULL)");
-                    $stmt->execute(['studentNum'=>$studentNum, 'initials'=>$initials , 'lastname'=>$lastname, 'email'=>$email, 'contact'=>$contact, 'password'=>$password , 'condition'=>$condition, 'reason'=>$reasonExclusion, 'subjCode'=>$subjectCode, 'interviewed'=>$interviewed]);
-                    $_SESSION['success'] = 'Student added successfully';	
-					header('location: students.php');
-					exit();
+	try {
+		$stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows1 FROM student WHERE studentNum = :studentNumber");
+		$stmt->execute(['studentNumber' => $studentNum]);
+		$row = $stmt->fetch();
 
-				}
-				
-
-			
+		if ($row['numrows1'] > 0) {
+			$_SESSION['error'] = 'Student already Exists';
+			header('location: add-student-department.php');
+			exit();
+		} else {
+			$stmt = $conn->prepare("INSERT INTO `student` (`studentNum`,`initials`, `lastname`, `email`,`contactNr`, `password`, `condition_reAdmission`, `reason`, `subjCode`, `interviewedStatus`, `interviewDate`, `interviewRemarks`, `highSchool`, `Province`, `town` ) VALUES (:studentNum, :initials, :lastname, :email, :contact, :password, :condition, :reason, :subjCode, :interviewed, NULL, NULL, NULL, NULL, NULL)");
+			$stmt->execute(['studentNum' => $studentNum, 'initials' => $initials, 'lastname' => $lastname, 'email' => $email, 'contact' => $contact, 'password' => $password, 'condition' => $condition, 'reason' => $reasonExclusion, 'subjCode' => $subjectCode, 'interviewed' => $interviewed]);
+			$_SESSION['success'] = 'Student added successfully';
+			header('location: students.php');
+			exit();
 		}
-		catch(PDOException $e){
-			echo "There is some problem in connection: " . $e->getMessage();
-		}
-
+	} catch (PDOException $e) {
+		echo "There is some problem in connection: " . $e->getMessage();
 	}
-	else{
-		$_SESSION['error'] = 'Input Student form first';
-		//$_SESSION['success'] = 'Input login credentails first';
-	}
+} else {
+	$_SESSION['error'] = 'Input Student form first';
+	//$_SESSION['success'] = 'Input login credentails first';
+}
 
-	$pdo->close();
+$pdo->close();
 
-	header('location: add-student-department.php');
-
-?>
+header('location: add-student-department.php');
